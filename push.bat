9b1c803b85c5cf1e4eec0d4bf4f4e1bfb7fdfd61
SRM User <usuario@srm.app>
2026-09-23 22:49:03 +0200

    chore: add push.bat helper script

---
+++ push.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ========================================================
+echo   Subiendo cambios del SRM a GitHub (antoniomartin/srm)
+echo ========================================================
+git push origin main
+pause
